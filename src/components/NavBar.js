330f8b3c02b453bb980bddc3d968587ceb41b336
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-31 10:28:39 +0300

    learnt how to use keys and lists for props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksElement = links.map((link,index) => {
+    return <li key={index}><a href={`#${link}`}>{link}</a></li>
+  })
+  const style = {
+    display:"flex",
+    listStyleType: "none"
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  }
+
+  return <nav>{/* display an <a> tag for each link here */}
+  <ul style={style}>
+  {linksElement}
+  </ul></nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies)
+  const technologyItems = technologies.map((technology, index) => {
+    return <span key={index} className="technology">{technology}</span>;
+  });
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologyItems}
       </div>
     </div>
   );
